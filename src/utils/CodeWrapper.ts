2b24a3bbb8638e25f66b3282480aaec25f03fdcd
Gordon Clark <gordon.clark@botpress.com>
2023-08-14 16:02:16 +0800

    docstrings

--- src/utils/CodeWrapper.ts
+++ src/utils/CodeWrapper.ts
@@ -4,69 +4,95 @@ import {readFiles as __rf} from  './fileTools.js'
 import {writeFiles as __wf, FileData} from  './fileTools.js'; // Placeholder for your file writing module
 import Task from "./Task"
 import winston from 'winston';
-
+/**
+ * Wraps and runs the LLM-generated code.
+ * Provides methods for reading and writing files, executing the code, and logging errors.
+ */
 export default class CodeWrapper {
+  /** The code to run. */
   private codeString: string;
+  /** Logger for the task. */
   private logger: any;
-  private task:Task
+  /** The task object associated with the code. */
+  private task: Task;
 
-  constructor(codeString: string, task:Task) {
+  /**
+   * Creates a new instance of CodeWrapper.
+   * @param codeString The code to be executed.
+   * @param task The task object associated with the code.
+   */
+  constructor(codeString: string, task: Task) {
     this.codeString = codeString;
-    this.task = task
+    this.task = task;
     this.logger = winston.createLogger({
-        level: "debug",
-        format: winston.format.combine(
-          winston.format.timestamp(),
-          winston.format.colorize(),
-          winston.format.json(),
-        ),
-        defaultMeta: { service: "vr-router" },
-        transports: [
-          new winston.transports.File({
-            filename: `error.log`,
-            level: "error",
-          }),
-          new winston.transports.File({
-            filename: `combined.log`,
-            level: "debug",
-          }),
-        ],
-      });
-      if (process.env.NODE_ENV !== "production") {
-        this.logger.add(
-          new winston.transports.Console({
-            format: winston.format.combine(
-              winston.format.prettyPrint(),
-              winston.format.simple(),
-            ),
-          }),
-        );
-        }
+      level: "debug",
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.colorize(),
+        winston.format.json()
+      ),
+      defaultMeta: { service: "vr-router" },
+      transports: [
+        new winston.transports.File({
+          filename: `error.log`,
+          level: "error",
+        }),
+        new winston.transports.File({
+          filename: `combined.log`,
+          level: "debug",
+        }),
+      ],
+    });
+    if (process.env.NODE_ENV !== "production") {
+      this.logger.add(
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.prettyPrint(),
+            winston.format.simple()
+          ),
+        })
+      );
+    }
     // Any other initializations can go here
   }
-  writeFiles(fileData:FileData[]){
-    console.log(`Writing file: ${JSON.stringify(fileData)}`)
-    return __wf(fileData, this.task)
+
+  /**
+   * Writes files to the specified location.
+   * @param fileData An array of FileData objects containing the file path and data.
+   * @returns A string indicating the result of the file writing operation.
+   */
+  writeFiles(fileData: FileData[]): string {
+    console.log(`Writing file: ${JSON.stringify(fileData)}`);
+    return __wf(fileData, this.task);
   }
-  readFiles(paths:string[]){
-    var updatedPaths:string[] = []
-    for(let path of paths){
-        updatedPaths.push(
-            `./repos/${this.task.owner}/${this.task.repo}/${this.task.branchName}/${path}`
-        )
+
+  /**
+   * Reads files from the specified location.
+   * @param paths An array of file paths to be read.
+   * @returns A promise that resolves to a string containing the contents of the files.
+   */
+  readFiles(paths: string[]): Promise<string> {
+    var updatedPaths: string[] = [];
+    for (let path of paths) {
+      updatedPaths.push(
+        `./repos/${this.task.owner}/${this.task.repo}/${this.task.branchName}/${path}`
+      );
     }
-    return __rf(updatedPaths, this.task.id)
+    return __rf(updatedPaths, this.task.id);
   }
+
+  /**
+   * Executes the code stored in the codeString property.
+   * @returns A promise that resolves to a string indicating that the code ran successfully,
+   * or rejects with an error message if there is an error during code execution.
+   */
   async executeCode(): Promise<string> {
     try {
-        
-     
-     //console.log(this.codeString)
-      eval(this.codeString)//.call(this);
-      return new Promise(resolve=> resolve("Code ran successfully"))
+      eval(this.codeString);
+      return new Promise((resolve) => resolve("Code ran successfully"));
     } catch (error) {
       this.logger.error(`Error executing code: ${error}`);
-      return new Promise(reject=>reject(`Error running code: ${error}`))
+      return new Promise((reject) => reject(`Error running code: ${error}`));
     }
   }
 }
--- src/utils/fileTools.ts
+++ src/utils/fileTools.ts
@@ -6,15 +6,21 @@ export interface FileData {
     path: string;
     data: string;
   }
-  function popFirstDir(filepath:string) {
-    const normalPath = path.normalize(filepath)
-    const parts = normalPath.split(path.sep);
+/**
+ * Removes the first directory from a file path.
+ * 
+ * @param {string} filepath - The file path to modify.
+ * @returns {string} The modified file path without the first directory.
+ */
+function popFirstDir(filepath:string) {
+  const normalPath = path.normalize(filepath);
+  const parts = normalPath.split(path.sep);
 
-    // Remove the first directory
-    parts.shift();
+  // Remove the first directory
+  parts.shift();
 
-    // Join the parts back into a path
-    return parts.join(path.sep);
+  // Join the parts back into a path
+  return parts.join(path.sep);
 }
 /**
  * Retrieves the contents of multiple files specified by their file paths.
@@ -50,44 +56,57 @@ export function readFiles(paths: string[], taskId: string): Promise<string> {
         });
     });
   }
- export function writeFiles(fileData: FileData[], task:Task): string {
-    let resp = "";
-    var updatedFilepath = ""
-    const {owner, repo, branchName, id} = task
-    const promises = fileData.map((file) => {
-      if (/\.\/repos/.test(file.path)) {
-        updatedFilepath = popFirstDir(file.path)
-      }else{
-        updatedFilepath = file.path
-      }
-      console.log(`HERE IS THE FILE DATA BEING WRITTEN:\n\n${file.data}`)
-      const fileWriteConfig:AxiosRequestConfig={
-        method:'POST',
-        url:`${endpoint}/files/${owner}/${repo}/${branchName}/writeFile`,
-        headers:{
-          "Content-Type":"application/json"
-        },
-        params:{owner: owner,
-          repo: repo,
-          branchName: branchName,},
-          data:{ filePath: "./"+updatedFilepath,
-            data: file.data,
-            taskId: id,}
-      }
-      return axios(fileWriteConfig);
+/**
+ * Writes files to the specified paths using a POST request.
+ * 
+ * @param fileData - An array of objects representing the files to be written. Each object should have a `path` property indicating the path where the file should be written, and a `data` property containing the content of the file.
+ * @param task - An object containing information about the task being performed, including the `owner` (repository owner), `repo` (repository name), `branchName` (name of the branch), and `id` (task ID).
+ * @returns A string indicating whether the files were successfully written or if there was an error.
+ */
+export function writeFiles(fileData: FileData[], task: Task): string {
+  let resp = "";
+  var updatedFilepath = ""
+  const {owner, repo, branchName, id} = task
+  const promises = fileData.map((file) => {
+    if (/\.\/repos/.test(file.path)) {
+      updatedFilepath = popFirstDir(file.path)
+    }else{
+      updatedFilepath = file.path
+    }
+    const fileWriteConfig:AxiosRequestConfig={
+      method:'POST',
+      url:`${endpoint}/files/${owner}/${repo}/${branchName}/writeFile`,
+      headers:{
+        "Content-Type":"application/json"
+      },
+      params:{owner: owner,
+        repo: repo,
+        branchName: branchName,},
+        data:{ filePath: "./"+updatedFilepath,
+          data: file.data,
+          taskId: id,}
+    }
+    return axios(fileWriteConfig);
+  });
+  Promise.all(promises)
+    .then((responses) => {
+      console.log(responses)
+      resp = `Files successfully written:\n${responses}`;
+    })
+    .catch((err) => {
+      console.error(err)
+      resp = `Error writing files: ${err}`;
     });
-    Promise.all(promises)
-      .then((responses) => {
-        console.log(responses)
-        resp = `Files successfully written:\n${responses}`;
-      })
-      .catch((err) => {
-        console.error(err)
-        resp = `Error writing files: ${err}`;
-      });
-    return resp;
-  }
- export function submit(data: string, task:Task): string {
-    const resp = writeFiles([{ path: "/tmp/tmp.js", data: data }], task);
-    return resp;
-  }
+  return resp;
+}
+/**
+ * Submits the given data as a file for writing.
+ * 
+ * @param data The content of the file to be written.
+ * @param task The task associated with the file.
+ * @returns A string indicating the response from the file writing operation.
+ */
+export function submit(data: string, task: Task): string {
+  const resp = writeFiles([{ path: "/tmp/tmp.js", data: data }], task);
+  return resp;
+}
